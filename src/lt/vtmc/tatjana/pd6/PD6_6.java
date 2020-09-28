8a44be0d0113433cc75d248f4fc8e72c085887f4
Tatjana Grigaitiene <tatjana.grigaitiene@yahoo.com>
2020-09-28 21:47:00 +0300

    2020-09-28 21:46

--- src/lt/vtmc/tatjana/pd6/PD6_6.java
+++ src/lt/vtmc/tatjana/pd6/PD6_6.java
@@ -5,24 +5,22 @@ import java.util.Scanner;
 public class PD6_6 {
 
 	public static void main(String[] args) {
-		
-			Scanner dataInput = new Scanner(System.in);
 
-			System.out.print("Kiek žuvų gyvena akvariume? ");
-			int a = dataInput.nextInt();
+		Scanner dataInput = new Scanner(System.in);
 
-			System.out.print("Kiek žuvų į akvariumą įdedama kiekvieną dieną? ");
-			int b = dataInput.nextInt();
-			
-			System.out.print("Kiek dienų praėjo? ");
-			int n = dataInput.nextInt();
-			
-			dataInput.close();
+		System.out.print("Kiek žuvų gyvena akvariume? ");
+		int a = dataInput.nextInt();
 
-			int result = a+b*n;
-			System.out.println("Po " + n + " dienų akvariume gyvens " + result + " žuvų.");
-			
+		System.out.print("Kiek žuvų į akvariumą įdedama kiekvieną dieną? ");
+		int b = dataInput.nextInt();
 
+		System.out.print("Kiek dienų praėjo? ");
+		int n = dataInput.nextInt();
+
+		dataInput.close();
+
+		int result = a + b * n;
+		System.out.println("Po " + n + " dienų akvariume gyvens " + result + " žuvų.");
 
 	}
 
