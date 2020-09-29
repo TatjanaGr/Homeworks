eaa0946e0f06411edb1d5f50e17b36ce63d93a32
Tatjana Grigaitiene <tatjana.grigaitiene@yahoo.com>
2020-09-29 19:20:35 +0300

    2020-09-29 19:20

--- src/lt/vtmc/tatjana/pd7/PD7_1.java
+++ src/lt/vtmc/tatjana/pd7/PD7_1.java
@@ -1,10 +1,32 @@
 package lt.vtmc.tatjana.pd7;
 
+import java.util.Scanner;
+
 public class PD7_1 {
+	private static Scanner getData = new Scanner(System.in);
+	private static int treeNumber = 0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		getTreeNumber();
+		double result = getTreeHeight();
+		System.out.println("Eglutės aukščio vidurkis: " + result / treeNumber + " cm");
 	}
 
+	public static void getTreeNumber() {
+		System.out.print("Kiek eglučių atvežta?   ");
+		treeNumber = getData.nextInt();
+	}
+
+	public static double getTreeHeight() {
+		double result = 0;
+		for (int i = 0; i < treeNumber; i++) {
+			System.out.print("Įveskite eglutės aukštį, cm:   ");
+			double treeHeight = getData.nextDouble();
+			if (treeHeight > 0) {
+				result += treeHeight;
+			}
+		}
+		// System.out.println("Bendras eglučių aukštis " + result + " cm");
+		return result;
+	}
 }
