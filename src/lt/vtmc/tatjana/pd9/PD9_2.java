078ddc10a4c62a2a0d6915300ea54623810b705a
Tatjana Grigaitiene <tatjana.grigaitiene@yahoo.com>
2020-09-30 19:47:54 +0300

    2020-09-30 19:47

--- src/lt/vtmc/tatjana/pd9/PD9_2.java
+++ src/lt/vtmc/tatjana/pd9/PD9_2.java
@@ -1,35 +1,14 @@
 package lt.vtmc.tatjana.pd9;
 
 public class PD9_2 {
-	static double[] january = new double[6];
-	static double[] february = new double[6];
-	static double[] march = new double[6];
+	static double[] january = {-11.5, -10.3, -12, -6.5, -5, -15.5};
+	static double[] february = {-16, -14.5, -11, -10.3, -3.5, 0};
+	static double[] march = {-3, -2.5, 0, 5, 9.5, 11.2};
 	static double sumJan = 0;
 	static double sumFeb = 0;
 	static double sumMar = 0;
 
 	public static void main(String[] args) {
-
-		january[0] = -11.5;
-		january[1] = -10.3;
-		january[2] = -12;
-		january[3] = -6.5;
-		january[4] = -5;
-		january[5] = -15.5;
-
-		february[0] = -16;
-		february[1] = -14.5;
-		february[2] = -11;
-		february[3] = -10.3;
-		february[4] = -3.5;
-		february[5] = 0;
-
-		march[0] = -3;
-		march[1] = -2.5;
-		march[2] = -0;
-		march[3] = 5;
-		march[4] = 9.5;
-		march[5] = 11.2;
 
 		// System.out.println("\n" + Arrays.toString(january));
 		// System.out.println("\n" + Arrays.toString(february));
