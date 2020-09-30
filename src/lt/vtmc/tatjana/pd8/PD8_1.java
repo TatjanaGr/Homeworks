65281384ae0750ae15a49c8612f79a3556110324
Tatjana Grigaitiene <tatjana.grigaitiene@yahoo.com>
2020-09-30 15:22:00 +0300

    2020-09-30 15:21

--- src/lt/vtmc/tatjana/pd8/PD8_1.java
+++ src/lt/vtmc/tatjana/pd8/PD8_1.java
@@ -1,9 +1,29 @@
 package lt.vtmc.tatjana.pd8;
 
 public class PD8_1 {
+	static double[] price = new double[6];
+	static double sum = 0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		price[0] = 102.56;
+		price[1] = 215.72;
+		price[2] = 99.21;
+		price[3] = 200;
+		price[4] = 175.99;
+		price[5] = 214.99;
+
+		//for (double i : price) {
+		//	System.out.printf("%s\n", i + "    ");
+		//}
+
+		for (int i = 0; i < price.length; i++)
+			sum = sum + price[i];
+		double average = sum / price.length;
+		System.out.print("Vidutiniškai viena prekė kainavo  ");
+		System.out.printf("%.2f", average);
+		System.out.println(" Eur");
+		System.out.println("Per dieną pardavė prekių už   " + sum + " Eur");
 
 	}
 
